68e9408b309751acb1df7c14d4eb84f20183c633
muqg <muqglu@gmail.com>
2016-08-25 01:32:59 +0300

    Add files via upload

---
+++ Battles/Battles/Items/Common/Level 1/NefRune.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Battles.Items.Common
+{
+    [Serializable]
+    sealed class NefRune : Item
+    {
+        private const string name = "Nef Rune";
+
+        public NefRune()
+            : base(name)
+        {
+            // TODO:
+        }
+    }
+}
